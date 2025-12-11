199a740920d954387793b912402fe7792bbe81b1
Nick Allen <nallen05@gmail.com>
2025-12-11 15:12:08 -0800

    fixing subtle bug in :ENFORCE-COOLOFF

--- flokkr/flokkr.lisp
+++ flokkr/flokkr.lisp
@@ -293,7 +293,7 @@
         ;; (4) compute how long to wait
         :compute-wait (lambda (&aux ,next-wait)
                         ,@(nreverse compute-wait-body)
-                        ,next-wait)))))
+                        (max ,next-wait 0))))))
 
 (defun %flokkr-run (flok &aux flok-start-itu last-tick-start-itu)
   (loop do
